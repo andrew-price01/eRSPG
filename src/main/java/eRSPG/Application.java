ae199ce4bea352d60377c5e1badc99b451b7e242
Alex Gabello <gabelloster@gmail.com>
2015-11-04 19:20:19 -0700

    Added configuration for hibernate 4

--- src/main/java/eRSPG/Application.java
+++ src/main/java/eRSPG/Application.java
@@ -1,10 +1,19 @@
 package eRSPG;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -29,4 +38,45 @@ public class Application extends SpringBootServletInitializer {
     	resolver.setSuffix(".jsp");
     	return resolver;
     }
+    
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/erspg");
+        dataSource.setUsername("erspg_admin");
+        dataSource.setPassword("P@$$w0rd");
+     
+        return dataSource;
+    }
+    
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) {
+     
+        LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+     
+        //sessionBuilder.addAnnotatedClasses(User.class);
+        sessionBuilder.scanPackages("eRPSG.model");
+        sessionBuilder.addProperties(getHibernateProperties());
+        return sessionBuilder.buildSessionFactory();
+    }
+    
+    private Properties getHibernateProperties() {
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        return properties;
+    }
+    
+    @Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(
+            SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+                sessionFactory);
+     
+        return transactionManager;
+    }
+    
 }
