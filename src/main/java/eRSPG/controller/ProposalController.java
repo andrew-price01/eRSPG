bbb7b68740f585c003aa076cd0a3edcbf3fde047
jrnipko <jrnipko@gmail.com>
2015-11-11 17:43:32 -0700

    Initial Attachment Upload Changes

--- src/main/java/eRSPG/controller/ProposalController.java
+++ src/main/java/eRSPG/controller/ProposalController.java
@@ -1,8 +1,19 @@
 package eRSPG.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import eRSPG.Repository.FileUploadDAO;
+import eRSPG.model.UploadFile;
 
 @Controller
 public class ProposalController {
@@ -36,12 +47,43 @@ public class ProposalController {
 		return "projectIndex";
 	}
 	
-	@RequestMapping("/proposal/upload")
+	@RequestMapping(value="/proposal/upload",method=RequestMethod.GET)
 	public String uploadForm(Model model){
 		String contentPage = "proposalUpload.jsp";
 		model.addAttribute("contentPage",contentPage);
 		return "projectIndex";
 	}
+	
+	@RequestMapping(value="/proposal/upload",method=RequestMethod.POST)
+	public String saveupload(Model model){
+		String contentPage = "proposalUpload.jsp";
+		model.addAttribute("contentPage",contentPage);
+		return "projectIndex";
+	}
+	
+	@Autowired
+    private FileUploadDAO fileUploadDao;
+	
+    @RequestMapping(value = "/proposal/doUpload", method = RequestMethod.POST)
+    public String handleFileUpload(HttpServletRequest request,
+            @RequestParam CommonsMultipartFile[] fileUpload) throws Exception {
+          
+        if (fileUpload != null && fileUpload.length > 0) {
+            for (CommonsMultipartFile aFile : fileUpload){
+                  
+                System.out.println("Saving file: " + aFile.getOriginalFilename());
+                 
+                UploadFile uploadFile = new UploadFile();
+                uploadFile.setFileName(aFile.getOriginalFilename());
+                uploadFile.setData(aFile.getBytes());
+                fileUploadDao.save(uploadFile);               
+            }
+        }
+  
+        return "Success";
+    }  
+    
+   
 	
 	@RequestMapping("/proposal/review")
 	public String reviewForm(Model model){
