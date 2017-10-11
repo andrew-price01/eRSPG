37cc66bb8639b720fd3a80b7386b3835f18edb62
Yong-W530-PC\Yong-W530 <yongzhang@weber.edu>
2017-10-11 16:57:03 -0600

    rewrite spring configuration code to remove spring boot.

--- src/main/java/eRSPG/config/DataConfig.java
+++ src/main/java/eRSPG/config/DataConfig.java
@@ -1,24 +1,14 @@
-package eRSPG;
+package eRSPG.config;
 
-import java.util.Properties;
-
-import javax.sql.DataSource;
-
-import eRSPG.Repository.*;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
 
 import eRSPG.model.AwardType;
 import eRSPG.model.Awarded;
@@ -39,30 +29,17 @@ import eRSPG.model.SourceType;
 import eRSPG.model.UploadFile;
 import eRSPG.model.User;
 import eRSPG.model.UserRole;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@EnableWebMvc
+import java.util.Properties;
+import eRSPG.Repository.*;
+
+@Configuration
 @EnableTransactionManagement
-@SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+@EnableWebMvc
+public class DataConfig {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-    }
-
-    @Bean
-    public InternalResourceViewResolver getInternalResourceViewResolver()
-    {
-    	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-    	resolver.setPrefix("/WEB-INF/views/");
-    	resolver.setSuffix(".jsp");
-    	return resolver;
-    }
-    
     @Bean(name = "dataSource")
     public DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
@@ -70,88 +47,88 @@ public class Application extends SpringBootServletInitializer {
         dataSource.setUrl("jdbc:mysql://localhost:3306/erspg");
         dataSource.setUsername("erspg_admin");
         dataSource.setPassword("P@$$w0rd");
-     
+
         return dataSource;
     }
-    
+
     @Autowired
     @Bean(name = "sessionFactory")
     public SessionFactory getSessionFactory(DataSource dataSource) {
-     
+
         LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-     
+
         sessionBuilder.addAnnotatedClasses(Awarded.class, AwardType.class, Department.class, EssayAnswer.class, EssayQuestion.class, Fund.class, FundCategory.class,
-        									FundType.class, Participant.class, ProjectType.class, Proposal.class, RequestAward.class, RoleType.class,
-        									Semester.class, Reviewer.class, SourceType.class, UploadFile.class, User.class, UserRole.class);
+                FundType.class, Participant.class, ProjectType.class, Proposal.class, RequestAward.class, RoleType.class,
+                Semester.class, Reviewer.class, SourceType.class, UploadFile.class, User.class, UserRole.class);
         //sessionBuilder.scanPackages("eRPSG.model");
         sessionBuilder.addProperties(getHibernateProperties());
         return sessionBuilder.buildSessionFactory();
     }
-    
+
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
         properties.put("hibernate.show_sql", "true");
         properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return properties;
     }
-    
+
     @Autowired
     @Bean(name = "transactionManager")
     public HibernateTransactionManager getTransactionManager(
             SessionFactory sessionFactory) {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager(
                 sessionFactory);
-     
+
         return transactionManager;
     }
-    
+
     @Autowired
     @Bean(name = "proposalDao")
     public ProposalDAO getProposalDao(SessionFactory sessionFactory) {
         return new ProposalImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name = "awardTypeDao")
     public AwardTypeDAO getAwardTypeDao(SessionFactory sessionFactory) {
         return new  AwardTypeImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "requestAwardDao")
     public RequestAwardDAO getRequestAwardDao(SessionFactory sessionFactory){
-    	
-    	return new RequestAwardImpl(sessionFactory);
+
+        return new RequestAwardImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "departmentDAO")
     public DepartmentDAO getDepartmentDao(SessionFactory sessionFactory){
-    	
-    	return new DepartmentImpl(sessionFactory);
+
+        return new DepartmentImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "semesterDAO")
     public SemesterDAO getSemesterDao(SessionFactory sessionFactory){
-    	
-    	return new SemesterImpl(sessionFactory);
+
+        return new SemesterImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "fundDAO")
     public FundImpl getFundDao(SessionFactory sessionFactory){
-    	
-    	return new FundImpl(sessionFactory);
+
+        return new FundImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "essayAnswerDAO")
     public EssayAnswerDAO getEssayAnswerDao(SessionFactory sessionFactory){
-    	
-    	return new EssayAnswerImpl(sessionFactory);
+
+        return new EssayAnswerImpl(sessionFactory);
     }
-    
+
     @Autowired
     @Bean(name= "fileUploadDAO")
     public FileUploadDAO getFileUploadDao(SessionFactory sessionFactory){
@@ -162,4 +139,5 @@ public class Application extends SpringBootServletInitializer {
     @Autowired
     @Bean(name= "userDAO")
     public UserDAO getUserDao(SessionFactory sessionFactory){ return new UserImpl(sessionFactory); }
+
 }
---
+++ src/main/java/eRSPG/config/ERSPGWebAppInitializer.java
@@ -0,0 +1,24 @@
+package eRSPG.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ERSPGWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+    @Override
+    protected String[] getServletMappings()
+    {
+        return new String[] {"/"};
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses()
+    {
+        return new Class<?>[] { RootConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { WebConfig.class };
+    }
+
+}
