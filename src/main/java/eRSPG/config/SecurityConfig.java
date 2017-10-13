9a77b29fd5d2f198060300a9418ba0e04ee3aa1b
Yong Zhang <yongzhang@weber.edu>
2017-10-13 09:04:35 -0600

    add a testing user account.

--- src/main/java/eRSPG/config/SecurityConfig.java
+++ src/main/java/eRSPG/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package eRSPG.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user").password("password").authorities("ROLE_USER").and()
+            .withUser("admin").password("password").authorities("ROLE_USER", "ROLE_ADMIN");
+    }
 
 
 }
