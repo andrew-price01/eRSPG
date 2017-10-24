388275c12ad7761ba3510f10e44f95ab21121438
rick <mindtaker63@gmail.com>
2017-10-23 20:10:45 -0600

    optimizing imports

--- src/main/java/eRSPG/controller/HomeController.java
+++ src/main/java/eRSPG/controller/HomeController.java
@@ -1,10 +1,7 @@
 package eRSPG.controller;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
