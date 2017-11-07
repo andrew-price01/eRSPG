0031c13e4623949fd61f23cccf1d50bad8e95bfd
Devon Taylor <devontaylor00@gmail.com>
2017-11-06 18:20:48 -0700

    Add test case for reviewer controller

---
+++ src/test/java/eRSPG/controller/ReviewerControllerTest.java
@@ -0,0 +1,21 @@
+package eRSPG.controller;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceView;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class ReviewerControllerTest {
+
+	@Test
+	public void showAnnouncements() throws Exception {
+		ReviewerController controller = new ReviewerController();
+		MockMvc mockMvc = standaloneSetup(controller)
+				.setSingleView(new InternalResourceView("/WEB-INF/views/announcements.jsp"))
+				.build();
+		mockMvc.perform(get("/chairman/userManagement")).andExpect(view().name("projectIndex"));
+	}
+}
