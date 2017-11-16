fa5e626283d98016fccd63dc0d2fb632d2c9e686
David Mariluch <daddymooch@gmail.com>
2017-11-15 22:17:12 -0700

    Got CAS working with just the WAR file Will create new document

--- src/main/java/eRSPG/config/Constants.java
+++ src/main/java/eRSPG/config/Constants.java
@@ -18,10 +18,13 @@ public final class Constants {
 //    public static final String CAS_URL_LOGOUT = "https://casdev.weber.edu/logout";
 //    public static final String CAS_URL_PREFIX = "https://casdev.weber.edu";
 //    public static final String CAS_SERVICE_URL = "https://cas.weber.edu/p3/serviceValidate";
-    public static final String APP_SERVER = "http://localhost:8080";
-    public static final String CAS_SERVICE_URL = "http://localhost:8080/eRSPG/home";
-    public static final String CAS_URL_LOGIN = "https://localhost:8443/cas/login";
-    public static final String CAS_URL_LOGOUT = "https://localhost:8443/cas/logout";
-    public static final String CAS_URL_PREFIX = "https://localhost:8443/cas";
+    public static final String APP_SERVER = "http://localhost:8081";
+    public static final String CAS_SERVICE_URL = "http://localhost:8081/eRSPG/home";
+    public static final String CAS_URL_LOGIN = "https://localhost:8443/cas-server-webapp-3.5.1/login";
+    public static final String CAS_URL_LOGOUT = "https://localhost:8443/cas-server-webapp-3.5.1/logout";
+    public static final String CAS_URL_PREFIX = "https://localhost:8443/cas-server-webapp-3.5.1";
+//    public static final String CAS_URL_LOGIN = "https://localhost:8443/cas/login";
+//    public static final String CAS_URL_LOGOUT = "https://localhost:8443/cas/logout";
+//    public static final String CAS_URL_PREFIX = "https://localhost:8443/cas";
 
 }
