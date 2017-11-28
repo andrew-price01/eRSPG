ddd54ff2683673b450babc356261cdb8551736e4
David Mariluch <daddymooch@gmail.com>
2017-11-28 01:41:42 -0700

    Changed URL's to Weber's Cas System's URLs

--- src/main/java/eRSPG/config/Constants.java
+++ src/main/java/eRSPG/config/Constants.java
@@ -16,13 +16,13 @@ public final class Constants {
 
     /*  These Constants Are for use with the application when using Weber's CAS */
 
-//    public static final String APP_SERVER = "http://localhost:8081";
-//    public static final String CAS_URL_LOGIN = "https://casdev.weber.edu/login";
-//    public static final String CAS_URL_LOGOUT = "https://casdev.weber.edu/logout";
-//    public static final String CAS_URL_PREFIX = "https://casdev.weber.edu";
-//    public static final String CAS_SERVER = "http://localhost:8443";
-//    public static final String CAS_SERVICE_URL = "http://localhost:8081/eRSPG/home";
-//    public static final String CAS_SECURITY_SERVICE = "https://localhost:8443/cas-server-webapp-3.5.1/j_spring_cas_security_check";
+    public static final String APP_SERVER = "http://localhost:8081";
+    public static final String CAS_URL_LOGIN = "https://casdev.weber.edu/login";
+    public static final String CAS_URL_LOGOUT = "https://casdev.weber.edu/logout";
+    public static final String CAS_URL_PREFIX = "https://casdev.weber.edu";
+    public static final String CAS_SERVER = "https://casdev.weber.edu";
+    public static final String CAS_SERVICE_URL = "http://localhost:8081/eRSPG/home";
+    public static final String CAS_SECURITY_SERVICE = "https://localhost:8443/cas-server-webapp-3.5.1/j_spring_cas_security_check";
 
     /*  These Constants Are for use with the CAS WAR OVERLAY */
 //    public static final String APP_SERVER = "http://localhost:8080";
@@ -34,11 +34,11 @@ public final class Constants {
 //    public static final String CAS_SECURITY_SERVICE = "https://localhost:8443/cas-server-webapp-3.5.1/j_spring_cas_security_check";
 
     /*  These Constants Are for use with the CAS-SERVER-WEBAPP-3.5.1 */
-    public static final String APP_SERVER = "http://localhost:8081";
-    public static final String CAS_SERVICE_URL = "http://localhost:8081/eRSPG/home";
-    public static final String CAS_URL_LOGIN = "https://localhost:8443/cas-server-webapp-3.5.1/login";
-    public static final String CAS_URL_LOGOUT = "https://localhost:8443/cas-server-webapp-3.5.1/logout";
-    public static final String CAS_URL_PREFIX = "https://localhost:8443/cas-server-webapp-3.5.1";
-    public static final String CAS_SERVER= "http://localhost:8443";
-    public static final String CAS_SECURITY_SERVICE = "https://localhost:8443/cas-server-webapp-3.5.1/j_spring_cas_security_check";
+//    public static final String APP_SERVER = "http://localhost:8081";
+//    public static final String CAS_SERVICE_URL = "http://localhost:8081/eRSPG/home";
+//    public static final String CAS_URL_LOGIN = "https://localhost:8443/cas-server-webapp-3.5.1/login";
+//    public static final String CAS_URL_LOGOUT = "https://localhost:8443/cas-server-webapp-3.5.1/logout";
+//    public static final String CAS_URL_PREFIX = "https://localhost:8443/cas-server-webapp-3.5.1";
+//    public static final String CAS_SERVER= "http://localhost:8443";
+//    public static final String CAS_SECURITY_SERVICE = "https://localhost:8443/cas-server-webapp-3.5.1/j_spring_cas_security_check";
 }
