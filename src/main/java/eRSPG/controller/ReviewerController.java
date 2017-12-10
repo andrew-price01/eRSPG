46e497afc4e769dc261094d53483c54a64ba447a
Devon Taylor <devontaylor00@gmail.com>
2017-12-09 20:58:52 -0700

    Fix reviewer controller path names

--- src/main/java/eRSPG/controller/ReviewerController.java
+++ src/main/java/eRSPG/controller/ReviewerController.java
@@ -7,21 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReviewerController {
 
-	@RequestMapping("/reviewer/announcements")
+	@RequestMapping("/eRSPG/reviewer/announcements")
 	public String announcements(Model model) {
 		String contentPage = "announcements.jsp";
 		model.addAttribute("contentPage", contentPage);
 		return "projectIndex";
 	}
 
-	@RequestMapping("/reviewer/proposalList")
+	@RequestMapping("/eRSPG/reviewer/proposalList")
 	public String proposalList(Model model) {
 		String contentPage = "proposalList.jsp";
 		model.addAttribute("contentPage", contentPage);
 		return "projectIndex";
 	}
 
-	@RequestMapping("/reviewer/reviewProposal")
+	@RequestMapping("/eRSPG/reviewer/reviewProposal")
 	public String review(Model model) {
 		String contentPage = "review.jsp";
 		model.addAttribute("contentPage", contentPage);
