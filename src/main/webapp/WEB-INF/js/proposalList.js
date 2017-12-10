14c778daac499250f138ca5156fb8c0610db8e8b
rick <mindtaker63@gmail.com>
2017-12-10 15:21:39 -0700

    fixed dates on proposal list

--- src/main/webapp/WEB-INF/js/proposalList.js
+++ src/main/webapp/WEB-INF/js/proposalList.js
@@ -13,13 +13,21 @@ const tableBuilder = (proposalList) => {
                 <td data-label="Project Director">${p.director}</td>
                 <td data-label="Department">${p.department}</td>
                 <td data-label="Proposal Year">${p.year}</td>
-                <td data-label="Submission Date">${moment().format("MM/DD/YYYY hh:mm A",p.submitDate)}</td>
+                <td data-label="Submission Date">${new Date(p.submitDate).toLocaleDateString('en-US',{
+                    year: '2-digit',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: true,
+                })}</td>
                 <td data-label="Status">${p.status}</td>
             </tr>
            `);
        });
    }
 };
+//                <td data-label="Submission Date">${moment().format("MM/DD/YYYY hh:mm A",p.submitDate)}</td>
 
 const fetchProposals = () => {
     jQuery.ajax({
